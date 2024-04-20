09dca5b7dc7d36172accfd6dea31919393d21304
Tony Smid <tony@graphaware.com>
2024-04-20 22:08:40 +0200

    init setup pre-commit-hook test

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <h1 class="underline font-bold text-3xl">
+      Hello world!
+    </h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
